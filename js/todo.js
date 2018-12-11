6b887557334622eda7b397aee860ce3db56fa3fc
liunn <genius0970890015@gmail.com>
2018-12-12 00:56:19 +0800

    製作點選圖片js

--- js/todo.js
+++ js/todo.js
@@ -4,6 +4,7 @@ if(document.getElementById("search").value!="")
     if (code==13)
     list();
 }
+
 function list(){
     //console.log(document.getElementById("search").value);
     /*var node=document.createElement("li");
@@ -13,13 +14,24 @@ function list(){
     var list=document.createElement("li");
     var image=document.createElement("img");
     var image2=document.createElement("img");
-    image.src="assets/circle.png";
     var value=document.getElementById("search").value;
     var textnode=document.createTextNode(value);
+    var allli=document.getElementsByTagName('li').length;
+    image.src="assets/circle.png";
+    image2.src="assets/close.png";
     list.appendChild(image);
     list.appendChild(textnode);
-    image2.src="assets/close.png";
     image2.setAttribute("class","img2");
     list.appendChild(image2);
+    list.setAttribute("id",allli);
+    image.setAttribute("onclick","javascript:imgclick(this);");
+    image2.setAttribute("onclick","javascript:imgclick(this);");
     document.getElementById("list").appendChild(list);
+}
+/*document.getElementById("list").addEventListener('click', function () {
+    console.log(this.getElementById);
+});*/
+function imgclick(obj){
+    console.log(obj);
+    obj.src="assets/check.png";
 }
