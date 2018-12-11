b47470ff37f891a5cba7e1fb53b8072b63fd0108
liunn <genius0970890015@gmail.com>
2018-12-11 22:01:49 +0800

    將圖片加上class

--- js/todo.js
+++ js/todo.js
@@ -6,10 +6,20 @@ if(document.getElementById("search").value!="")
 }
 function list(){
     //console.log(document.getElementById("search").value);
-    var node=document.createElement("li");
+    /*var node=document.createElement("li");
+    var value=document.getElementById("search").value;
+    var textnode=node.appendChild(document.createTextNode(value));
+    document.getElementById("list").appendChild(textnode);*/
+    var list=document.createElement("li");
+    var image=document.createElement("img");
+    var image2=document.createElement("img");
+    image.src="assets/circle.png";
     var value=document.getElementById("search").value;
     var textnode=document.createTextNode(value);
-    node.appendChild(textnode);
-    document.getElementById("list").appendChild(node);
-
+    list.appendChild(image);
+    list.appendChild(textnode);
+    image2.src="assets/close.png";
+    image2.setAttribute("class","img2");
+    list.appendChild(image2);
+    document.getElementById("list").appendChild(list);
 }
