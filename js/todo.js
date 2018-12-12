583bea8997d1b191e6a9e09c1f3f2e091fd91a0b
Peixuan <bphsuan@gmail.com>
2018-12-12 15:23:24 +0800

    id編號修正

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,11 @@ window.onload = function() {
     var box_content = document.getElementsByClassName("keyboxVal")[0]; //取得inputbox
     var ul = document.getElementById("List_ul"); //取得ul
     box_content.focus();
-    var cal;
+    var children_sum = 0;
     box_content.addEventListener("keyup", function(e) { //按enter即新增待辦事項
         if (e.target.value != "" && e.keyCode === 13) {
             var li = document.createElement("li"); //創建一個li
-            children_sum = ul.children.length + 1; //計算children li數
+            children_sum += 1; //id編號累加
             var checkbox = document.createElement('input');
             checkbox.type = "checkbox";
             checkbox.value = 1; //預設不勾
@@ -16,9 +16,7 @@ window.onload = function() {
             li.appendChild(checkbox); //再append li內容checkbox
             li.appendChild(img); //再append li內容 垃圾桶
             li.appendChild(document.createTextNode(e.target.value)); //再append li內容text
-
             li.setAttribute("id", "element" + children_sum); //設定此次新增的li id
-            // li.setAttribute("style", "overflow:hidden")
             checkbox.setAttribute("id", "check" + children_sum); //設定此次新增的li裡的checkbox id
             checkbox.setAttribute("style", "margin-left:2%;margin-right:5%;");
             checkbox.setAttribute("onclick", "textstyle_change(" + children_sum + ")"); //設定click執行之function
