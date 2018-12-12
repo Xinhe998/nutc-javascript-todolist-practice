8ccf88c5a35d69000d0871ef38e849e4b512e677
jingyi <amychen861110@gmail.com>
2018-12-12 20:30:57 +0800

    version2-新增待辦事項

--- js/todo.js
+++ js/todo.js
@@ -9,28 +9,30 @@ window.onload = function(){
             var checkbox = document.createElement("input");     //新增input
             var list = document.createElement("text");          //新增text
             var deletenode = document.createElement("span");    //新增span->X
-            checkbox.type = "checkbox";                         //給定input type為checkbox
+            checkbox.type = "checkbox";                         //set input type為checkbox
             list.innerHTML = content_text;                      //將字塞進text裡
             deletenode.innerHTML = "X";                         //X
             document.getElementById("memo").appendChild(node);  //增加一個li小孩
-            node.setAttribute("id",n);                          //塞id
+            node.setAttribute("id",n);                          //set li id
             node.appendChild(checkbox);                         //li裡的checkbox
             node.appendChild(list);                             //li裡的text
             node.appendChild(deletenode);                       //li裡的X
-            checkbox.setAttribute("id","check"+n); 
-            checkbox.setAttribute("onclick","done()");
-            list.setAttribute("id","list"+n);              
+            checkbox.setAttribute("id","check"+n);              //set checkbox id
+            checkbox.setAttribute("onclick","done()");          //set checkbox onclick
+            list.setAttribute("id","list"+n);                   //set list id
             n+=1;
+            document.getElementById("enter").value="";          //清空input
         }
-    };
-    function done(n){
-        var ischecked = document.getElementById("check"+n);
-        var change = document.getElementById("list"+n); 
-        if(ischecked.checked==true){
-            change.setAttribute("style","text-decoration=line-through");   
-            console.log("true");
-        };
-
-    };
-        
+    }; 
 };
+function done(n){
+    var ischecked = document.getElementById("check"+n);
+    var change = document.getElementById("list"+n); 
+    if(ischecked.checked){
+        change.setAttribute("style","text-decoration:line-through;");   
+        console.log("true");
+    }else{
+        change.setAttribute("style","text-decoration:none;");   
+        console.log("false");
+    }
+};    
