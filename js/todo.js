0861482ad6d8ef1b921e6a64be0f913d1fe9df3a
Tiffany <1102b004@gmail.com>
2018-12-13 15:33:13 +0800

    產生checkbox & delete button

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,20 @@
 function myFunction() {
     if(event.keyCode == 13) {
+    //checkbox
+    var check = document.createElement("INPUT");
+    check.setAttribute("type", "checkbox");
+    document.getElementById("dolist").appendChild(check);
+    //list
     var node = document.createElement("Li");
     var text =document.getElementById("todo").value;
     var textnode = document.createTextNode(text);
     node.appendChild(textnode);
     document.getElementById("dolist").appendChild(node);
+    //deleteButton
+    var btn = document.createElement("BUTTON");
+    var t = document.createTextNode("x");
+    btn.appendChild(t);
+    document.getElementById("dolist").appendChild(btn);
+
     }
 }
