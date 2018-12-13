5ce1c6be985458fb316a9e533d181ca32a4aa09c
ChiaAn <s1310731043@gms.nutc.edu.tw>
2018-12-13 18:42:55 +0800

    完成remove&刪除線

--- js/todo.js
+++ js/todo.js
@@ -1,10 +1,38 @@
 
 document.getElementById("text").addEventListener('keypress', function () {
     if (event.keyCode == 13) {
-        var li = document.createElement("li");
-        var li = document.createElement("button");
+        //var ul = document.createElement("ul");
+        //var li = document.createElement("li");
+        var checkbox = document.createElement("INPUT");
         var text = document.createTextNode(document.getElementById("text").value);
-        li.appendChild(text);
-        document.getElementById("lu").appendChild(li);
+        var tr =document.createElement("tr");
+        var td =document.createElement("td");
+        var span =document.createElement("span");
+        span.addEventListener('click',function() {
+            tr.remove();
+        });
+        checkbox.setAttribute("type", "checkbox");
+        document.getElementById("table").appendChild(tr);
+
+        checkbox.addEventListener('click',function() {
+            if (checkbox.checked==true){
+                td.classList.add("tdclass");
+            }
+            else {
+                td.classList.remove("tdclass");
+            }
+        })
+        
+        tr.appendChild(td);
+        td.appendChild(checkbox);
+        td.appendChild(text);
+        td.appendChild(span);
+        //td.appendChild(ul);
+        //ul.appendChild(li);
+        //li.appendChild(checkbox);
+        //li.appendChild(text);
+        //li.appendChild(span);
+        
+        document.getElementById("table").border = "1px solid #000000";
     }
 });
