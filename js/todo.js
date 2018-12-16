5902522584640e9dc1b3236ce18defc95d563320
LU <monkey07233@gmail.com>
2018-12-16 14:43:20 +0800

    完成todolist操作

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,20 @@
 function isEnter() {
     if (event.keyCode == 13) {
-        var node = document.createElement("LI");
-        var value = document.getElementById("content").value;
-        var textnode = document.createTextNode(value);
-        node.appendChild(textnode);
-        document.getElementById("list").appendChild(node);
+        var li = document.createElement("li");
+        var checkbox = document.createElement("input"); //打勾勾
+        checkbox.type = "checkbox";
+        checkbox.value = 1;
+        var label = document.createElement("label");  //輸入值
+        var button = document.createElement("button"); //刪除按鈕         
+        label.appendChild(document.createTextNode(document.getElementById("content").value));
+        button.appendChild(document.createTextNode("x"));
+        li.appendChild(checkbox);
+        li.appendChild(label);
+        li.appendChild(button);
+        document.getElementById("list").appendChild(li);
+        document.getElementById("content").value = "";
+        button.addEventListener("click", function () {
+            this.parentNode.remove();
+        });
     }
 }
